bb7124ee37e9a9c3bfe63eb33c13d8642e487f28
Daryll Santos <daryll.santos@gmail.com>
2022-01-16 00:09:31 +0800

    Minor Ui changes

--- libs/ui/src/lib/button/button.component.ts
+++ libs/ui/src/lib/button/button.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'angular-generals-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
   }
-
 }
