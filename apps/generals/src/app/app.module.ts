ab94f04e169cb4876794425e9995430fcea2f083
Daryll Santos <daryll.santos@gmail.com>
2022-01-18 00:31:38 +0800

    Add NgRx StoreDevtools, add to AppModule

--- apps/generals/src/app/app.module.ts
+++ apps/generals/src/app/app.module.ts
@@ -4,12 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { UiModule } from '@angular-generals/ui/ui.module';
+import { GameModule } from '@angular-generals/game';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, UiModule, BrowserAnimationsModule, MatSliderModule],
+  imports: [
+    BrowserModule,
+    UiModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument(),
+    GameModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
