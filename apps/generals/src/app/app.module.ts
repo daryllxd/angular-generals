8d5c7fcfc8595055d8c9f4a6c40e67f7ac26a741
Daryll Santos <daryll.santos@gmail.com>
2022-02-24 00:51:24 +0800

    Eslint, header, router

--- apps/generals/src/app/app.module.ts
+++ apps/generals/src/app/app.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { UiModule } from '@angular-generals/ui/ui.module';
 import { GameModule } from '@angular-generals/game';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     UiModule,
@@ -20,6 +21,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     StoreModule.forRoot({}),
     StoreDevtoolsModule.instrument(),
     GameModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [
-    ButtonComponent
-  ],
-  exports: [
-    ButtonComponent
-  ]
+  declarations: [ButtonComponent, HeaderComponent],
+  exports: [ButtonComponent, HeaderComponent],
 })
 export class UiModule {}
