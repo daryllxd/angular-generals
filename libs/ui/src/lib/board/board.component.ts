279edf8394d634eef3e85a9c4769758811784d1b
Daryll Santos <daryll.santos@gmail.com>
2022-02-25 17:40:25 +0800

    Simple board

---
+++ libs/ui/src/lib/board/board.component.ts
@@ -0,0 +1,56 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Board } from '../ui.module';
+
+@Component({
+  selector: 'angular-generals-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'],
+})
+export class BoardComponent implements OnInit {
+  @Input() rows = 8;
+  @Input() columns = 9;
+
+  board: Board = {
+    boardTiles: [],
+  };
+
+  ngOnInit(): void {
+    this.board = {
+      boardTiles: Array.from({ length: this.rows }, () =>
+        Array.from({ length: this.columns }, () => {
+          return { piece: null };
+        })
+      ),
+    };
+
+    this.board.boardTiles[0][1] = {
+      piece: {
+        player: {
+          name: 'Johny',
+        },
+        rank: 'sergeant',
+      },
+    };
+
+    this.board.boardTiles[2][1] = {
+      piece: {
+        player: {
+          name: 'Johny',
+        },
+        rank: 'sergeant',
+      },
+    };
+
+    this.board.boardTiles[2][2] = {
+      piece: {
+        player: {
+          name: 'Johny',
+        },
+        rank: 'sergeant',
+      },
+    };
+
+    console.log(this.board.boardTiles[0][1]);
+    console.log(this.board.boardTiles[1][1]);
+  }
+}
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { HeaderComponent } from './header/header.component';
+import { BoardComponent } from './board/board.component';
+
+export interface User {
+  name: string;
+}
+
+type rank = 'sergeant' | 'spy' | 'private' | 'flag';
+
+export interface Piece {
+  player: User;
+  rank: rank;
+}
+
+export interface BoardTile {
+  piece: Piece | null;
+}
+
+export interface Board {
+  boardTiles: BoardTile[][];
+}
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ButtonComponent, HeaderComponent],
-  exports: [ButtonComponent, HeaderComponent],
+  declarations: [ButtonComponent, HeaderComponent, BoardComponent],
+  exports: [BoardComponent, ButtonComponent, HeaderComponent],
 })
 export class UiModule {}
